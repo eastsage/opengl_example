6481350582c470bd9152923edc8fa0dbcbb8d6eb
eastsage <dpwk8@naver.com>
2021-05-17 23:31:58 +0900

    11week

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -67,16 +67,31 @@ MeshUPtr Mesh::CreateBox() {
         Vertex { glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(1.0f, 1.0f) },
         Vertex { glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
         Vertex { glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
-  };
+    };
 
-  std::vector<uint32_t> indices = {
+    std::vector<uint32_t> indices = {
         0,  2,  1,  2,  0,  3,
         4,  5,  6,  6,  7,  4,
         8,  9, 10, 10, 11,  8,
         12, 14, 13, 14, 12, 15,
         16, 17, 18, 18, 19, 16,
         20, 22, 21, 22, 20, 23,
-  };
+    };
+
+    return Create(vertices, indices, GL_TRIANGLES);
+}
+
+MeshUPtr Mesh::CreatePlane() {
+    std::vector<Vertex> vertices = {
+        Vertex { glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3( 0.0f,  0.0f, 1.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex { glm::vec3( 0.5f, -0.5f, 0.0f), glm::vec3( 0.0f,  0.0f, 1.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex { glm::vec3( 0.5f,  0.5f, 0.0f), glm::vec3( 0.0f,  0.0f, 1.0f), glm::vec2(1.0f, 1.0f) },
+        Vertex { glm::vec3(-0.5f,  0.5f, 0.0f), glm::vec3( 0.0f,  0.0f, 1.0f), glm::vec2(0.0f, 1.0f) },
+    };
+
+    std::vector<uint32_t> indices = {
+        0,  1,  2,  2,  3,  0,
+    };
 
     return Create(vertices, indices, GL_TRIANGLES);
 }
